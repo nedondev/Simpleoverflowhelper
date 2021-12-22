0a9a04444af6c0d08d7935275c8cdf1e885034d8
nedondev <nedondev@gmail.com>
2021-12-21 21:35:53 -0500

    mistype

--- simpleoverflowhelper.py
+++ simpleoverflowhelper.py
@@ -119,7 +119,7 @@ def gen3():
     print("./simpleoverflowhelper.py 4 -b \"0\"")
     print("./simpleoverflowhelper.py 4 -b \"0,a\"")
     print("!mona config -set workingfolder c:\\mona\\%p")
-    print("!mona bytearray -b \"\x00\"")
+    print("!mona bytearray -b \"\\x00\"")
     print("!mona compare -f C:\\mona\\malbec\\bytearray.bin -a <start-esp>")
     pass
 
